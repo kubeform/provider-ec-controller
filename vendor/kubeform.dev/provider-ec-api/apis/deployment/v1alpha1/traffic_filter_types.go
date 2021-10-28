fc4f3749b6840c99bb1e44a1798d2db12ecaa485
1gtm <1gtm@appscode.com>
2021-10-28 08:11:03 +0000

    Generate code for provider@v0.2.1 gen@6dbeccd
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-ec-api/apis/deployment/v1alpha1/traffic_filter_types.go
+++ vendor/kubeform.dev/provider-ec-api/apis/deployment/v1alpha1/traffic_filter_types.go
@@ -62,6 +62,8 @@ type TrafficFilterSpec struct {
 	TerminationPolicy base.TerminationPolicy `json:"terminationPolicy,omitempty" tf:"-"`
 
 	ProviderRef core.LocalObjectReference `json:"providerRef" tf:"-"`
+
+	BackendRef *core.LocalObjectReference `json:"backendRef,omitempty" tf:"-"`
 }
 
 type TrafficFilterSpecResource struct {
